5a519bbabbd210dd2e9b2282718772d56d7660bc
Koki <u2daijibrother@gmail.com>
2017-11-09 04:14:21 +0900

    divide cstorescp

--- Desktop/C-Store SCP/Program.cs
+++ Desktop/C-Store SCP/Program.cs
@@ -12,8 +12,7 @@ namespace Dicom.CStoreSCP
 {
     internal class Program
     {
-        private static string StoragePath = @".\DICOM";
-
+        
         private static void Main(string[] args)
         {
             // preload dictionary to prevent timeouts
@@ -25,123 +24,14 @@ namespace Dicom.CStoreSCP
             var port = args != null && args.Length > 0 && int.TryParse(args[0], out tmp) ? tmp : 11112;
             Console.WriteLine($"Starting C-Store SCP server on port {port}");
 
+            CStoreSCP.Initalize(@".\DICOM");
             var server = DicomServer.Create<CStoreSCP>(port);
-
-
+            
             // end process
             Console.WriteLine("Press <return> to end...");
             Console.ReadLine();
         }
 
-        private class CStoreSCP : DicomService, IDicomServiceProvider, IDicomCStoreProvider, IDicomCEchoProvider
-        {
-            private static DicomTransferSyntax[] AcceptedTransferSyntaxes = new DicomTransferSyntax[]
-                                                                                {
-                                                                                    DicomTransferSyntax
-                                                                                        .ExplicitVRLittleEndian,
-                                                                                    DicomTransferSyntax
-                                                                                        .ExplicitVRBigEndian,
-                                                                                    DicomTransferSyntax
-                                                                                        .ImplicitVRLittleEndian
-                                                                                };
-
-            private static DicomTransferSyntax[] AcceptedImageTransferSyntaxes = new DicomTransferSyntax[]
-                                                                                     {
-                                                                                         // Lossless
-                                                                                         DicomTransferSyntax
-                                                                                             .JPEGLSLossless,
-                                                                                         DicomTransferSyntax
-                                                                                             .JPEG2000Lossless,
-                                                                                         DicomTransferSyntax
-                                                                                             .JPEGProcess14SV1,
-                                                                                         DicomTransferSyntax
-                                                                                             .JPEGProcess14,
-                                                                                         DicomTransferSyntax
-                                                                                             .RLELossless,
-
-                                                                                         // Lossy
-                                                                                         DicomTransferSyntax
-                                                                                             .JPEGLSNearLossless,
-                                                                                         DicomTransferSyntax
-                                                                                             .JPEG2000Lossy,
-                                                                                         DicomTransferSyntax
-                                                                                             .JPEGProcess1,
-                                                                                         DicomTransferSyntax
-                                                                                             .JPEGProcess2_4,
-
-                                                                                         // Uncompressed
-                                                                                         DicomTransferSyntax
-                                                                                             .ExplicitVRLittleEndian,
-                                                                                         DicomTransferSyntax
-                                                                                             .ExplicitVRBigEndian,
-                                                                                         DicomTransferSyntax
-                                                                                             .ImplicitVRLittleEndian
-                                                                                     };
-
-            public CStoreSCP(INetworkStream stream, Encoding fallbackEncoding, Logger log)
-                : base(stream, fallbackEncoding, log)
-            {
-            }
-
-            public void OnReceiveAssociationRequest(DicomAssociation association)
-            {
-                if (association.CalledAE != "STORESCP")
-                {
-                    SendAssociationReject(
-                        DicomRejectResult.Permanent,
-                        DicomRejectSource.ServiceUser,
-                        DicomRejectReason.CalledAENotRecognized);
-                    return;
-                }
-
-                foreach (var pc in association.PresentationContexts)
-                {
-                    if (pc.AbstractSyntax == DicomUID.Verification) pc.AcceptTransferSyntaxes(AcceptedTransferSyntaxes);
-                    else if (pc.AbstractSyntax.StorageCategory != DicomStorageCategory.None) pc.AcceptTransferSyntaxes(AcceptedImageTransferSyntaxes);
-                }
-
-                SendAssociationAccept(association);
-            }
-
-            public void OnReceiveAssociationReleaseRequest()
-            {
-                SendAssociationReleaseResponse();
-            }
-
-            public void OnReceiveAbort(DicomAbortSource source, DicomAbortReason reason)
-            {
-            }
-
-            public void OnConnectionClosed(Exception exception)
-            {
-            }
-
-            public DicomCStoreResponse OnCStoreRequest(DicomCStoreRequest request)
-            {
-                var studyUid = request.Dataset.Get<string>(DicomTag.StudyInstanceUID);
-                var instUid = request.SOPInstanceUID.UID;
-
-                var path = Path.GetFullPath(Program.StoragePath);
-                path = Path.Combine(path, studyUid);
-
-                if (!Directory.Exists(path)) Directory.CreateDirectory(path);
-
-                path = Path.Combine(path, instUid) + ".dcm";
-
-                request.File.Save(path);
-
-                return new DicomCStoreResponse(request, DicomStatus.Success);
-            }
-
-            public void OnCStoreRequestException(string tempFileName, Exception e)
-            {
-                // let library handle logging and error response
-            }
-
-            public DicomCEchoResponse OnCEchoRequest(DicomCEchoRequest request)
-            {
-                return new DicomCEchoResponse(request, DicomStatus.Success);
-            }
-        }
+        
     }
 }
